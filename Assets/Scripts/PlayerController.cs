5438024e91119a751eb846be6d962e3d15f1db97
Hanul Rheem <41708091+violet-bl-cl@users.noreply.github.com>
2024-08-11 20:53:11 +1200

    add ground detection

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class PlayerController : InputManager
@@ -10,39 +7,53 @@ public class PlayerController : InputManager
     private float _movementSpeed = 0.0f;
     [SerializeField, Range(0.0f, 10.0f)]
     private float _jumpForce = 0.5f;
-    private Rigidbody2D _playerrb;
+    private CapsuleCollider2D _playerCapsule;
+    private float _playerHeight;
+    private Rigidbody2D _playerRb;
+    private bool _allowJump;
     [SerializeField]
     private float _groundRay = 0.5f;
     [SerializeField]
     private LayerMask _layerMasks;
-    // Start is called before the first frame update
     void Start()
     {
-        _playerrb = GetComponent<Rigidbody2D>();
+        _playerRb = GetComponent<Rigidbody2D>();
+        _playerCapsule = GetComponent<CapsuleCollider2D>();
+        _playerHeight = 1 / _playerCapsule.size.y;
     }
 
-    // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         if (horizontal != 0.0f)
         {
-            Vector2 newPosition = new Vector2((horizontal * 100 * _movementSpeed *Time.deltaTime),0f);
-            _playerrb.velocity = new Vector2(newPosition.x,_playerrb.velocity.y) ;
+            Vector2 newPosition = new Vector2((horizontal * 100 * _movementSpeed), 0f);
+            _playerRb.velocity = new Vector2(newPosition.x * Time.deltaTime, _playerRb.velocity.y);
         }
-        if (Input.GetKeyDown(JumpKey) ) //&& IsGround()
+
+        if (Input.GetKeyDown(JumpKey) && IsGround() && !_allowJump)
         {
+            _allowJump = true;
             Vector2 jumpAmount = Vector2.up * _jumpForce * 100 * Time.deltaTime;
-            _playerrb.velocity = jumpAmount;
+            _playerRb.velocity = jumpAmount;
         }
     }
     private bool IsGround()
     {
-        return Physics.Raycast(transform.up, Vector2.down, _groundRay, _layerMasks);
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, -Vector2.up, _groundRay + _playerHeight, _layerMasks);
+        if (hitInfo.collider != null)
+        {
+            Invoke(nameof(AllowJump) , 0.5f);
+            return true;
+        }
+        return false;
+    }
+    private void AllowJump(){
+        _allowJump = false;
     }
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.position, -transform.up * _groundRay);
+        Gizmos.DrawLine(transform.position, transform.position - new Vector3(0, _groundRay + _playerHeight, 0));
     }
 }
